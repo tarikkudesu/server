3f1ceb5886a436838ce3e69390f25318f795f1c1
Tarik Amehri <amehritarik@gmail.com>
2025-02-05 13:51:53 +0100

    Config file + php Cgi timeout script

--- cgi-bin/php/php.php
+++ cgi-bin/php/php.php
@@ -1,7 +1,15 @@
 <?php
-$var = 'curl -s -x GET http://localhost:9000/php.php';
+// Specify the full path to curl in case the path isn't set
+$curlPath = '/usr/bin/curl'; // Adjust path if necessary
+$var = "$curlPath -s http://localhost:9000/php/php.php";
 
+// Execute the command and capture both stdout and stderr
 $out = shell_exec($var);
 
-echo $out;
+// If there's no output, capture any errors
+if ($out === null) {
+    echo "Error executing command: " . $var;
+} else {
+    echo $out;
+}
 ?>
